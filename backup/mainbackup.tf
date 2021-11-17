a93b65ce4d66f1c1a4a2a1d56904aaeca5177aab
itorpu <itorpu@gmail.com>
2021-11-17 14:18:13 +0000

    end of lab 3 commit

---
+++ backup/mainbackup.tf
@@ -0,0 +1,39 @@
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 2.65"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+
+resource "azurerm_resource_group" "rg" {
+  name     = "myTFResourceGroup"
+  location = "uksouth"
+
+tags = {
+Environment = "Terraform Getting Started"
+Team = "DevOps"
+   }
+
+}
+
+
+# Create a virtual network
+resource "azurerm_virtual_network" "vnet" {
+    name                = "myTFVnet"
+    address_space       = ["10.0.0.0/16"]
+    location            = "uksouth"
+    resource_group_name = azurerm_resource_group.rg.name
+}
+
+
+
